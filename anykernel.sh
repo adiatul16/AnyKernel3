1a7616552d64205e4b02dba37607069defa73691
Aditya <adiatul16@gmail.com>
2020-11-10 18:53:17 +0530

    AK3: Adapt and set correct boot path

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,20 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=TaskMaster-EAS by Team Blacklisted
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=X00T
+device.name2=X00TD
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
